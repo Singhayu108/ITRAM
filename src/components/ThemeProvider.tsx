108ebaa9501724b7dfbe1b42c878c7481890f345
YOUR_GITHUB_USERNAME <domhacker66@gmail.com>
2025-06-02 00:09:22 -0700

    fix: add client directive and improve SSR handling

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,8 +1,13 @@
 'use client'
 
+import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <React.Suspense fallback={<div style={{ visibility: 'hidden' }}>{children}</div>}>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </React.Suspense>
+  )
 } 
